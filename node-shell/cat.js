8018779bcfd492e2283b95d780c605990e85500c
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-08-08 11:53:58 -0400

    Pushing all work completed together

--- node-shell/cat.js
+++ node-shell/cat.js
@@ -1,21 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = function () {
-    process.stdin.on('data', (data) => {
-      const cmd = data.toString().trim(); 
-      const cmd1 = cmd.split(' ')[0]
-      const filename = cmd.split(' ')[1]
-      process.stdout.write('\nprompt > ');
+  process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    const cmd1 = cmd.split(' ')[0];
+    const filename = cmd.split(' ')[1];
+    process.stdout.write('\nprompt > ');
     if (cmd1 === `cat`) {
       fs.readFile(`${filename}`, 'utf8', (err, data) => {
-    if (err) {
-        throw err
-    } else {
-        process.stdout.write(data)
-        process.stdout.write("prompt > ");
+        if (err) {
+          throw err;
+        } else {
+          process.stdout.write(data);
+          process.stdout.write('prompt > ');
+        }
+      });
     }
-}
-)
-    }
-}
-    )}
+  });
+};
--- node-shell/curl.js
+++ node-shell/curl.js
@@ -1,1 +1,25 @@
 const request = require('request')
+
+module.exports = function () {
+  process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    const cmd1 = cmd.split(' ')[0]
+    const filename = cmd.split(' ')[1]
+    process.stdout.write('\nprompt > ');
+  if (cmd1 === `curl`) {
+      request(filename, function (err, response, body) {
+        if (err) {
+          throw err;
+        }
+        // if (response) {
+        //   process.stdout.write(response)
+        // }
+        else {
+          process.stdout.write(response)
+          process.stdout.write(body);
+          process.stdout.write("prompt > ");
+        }
+      })
+    }
+  }
+)}
